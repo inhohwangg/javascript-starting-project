c8d3345a45c5903aa60a590b4b0dd2adbe86e7ca
inhohwangg <sk1440sk@naver.com>
2022-09-04 18:22:51 +0900

    .

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -25,6 +25,9 @@ function attackHandler() {
 }
 
 // 내가 만들어본 플레이어 공격 이벤트 함수
+// 작동은 된다
+// 하지만 이런식으로 함수를 늘려나가면
+// 가독성도 않좋고 불필요하게 코드의 길이가 늘어난다.
 // function playerAttackHandler() {
 //   const damage = dealPlayerDamage(ATTACK_VALUE);
 //   currentPlayerHealth -= damage;
