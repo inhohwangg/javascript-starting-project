1f669202c0ca895a7a921869bd738b308fbd0250
inhohwangg <sk1440sk@naver.com>
2022-09-08 14:15:30 +0900

    eventlistener

--- assets/scripts2/app2.js
+++ assets/scripts2/app2.js
@@ -1,5 +1,14 @@
-"use strict";
+const addListenerBtn = document.getElementById('add-listener-btn')
+const clickableBtn = document.getElementById('clickable-btn')
+const messageInput = document.getElementById('click-message-input')
 
-userName = "Max";
-let undefined = 5;
-console.log(userName);
+function printMessage() {
+    const value = messageInput.value
+    console.log(value || 'Click me!')
+}
+
+function addListener() {
+    clickableBtn.addEventListener('click', printMessage)
+}
+
+addListenerBtn.addEventListener('click', addListener)
