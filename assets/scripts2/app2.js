46fdf2556229040baf2c10a97cb03deb49fcc547
inhohwangg <sk1440sk@naver.com>
2022-09-10 18:38:55 +0900

    add

--- assets/scripts2/app2.js
+++ assets/scripts2/app2.js
@@ -1,8 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn')
 
-function startGame() {
-  console.log('Game is start...')
-}
+// const start = function() {
+//   console.log('Game is start...')
+// }
 
 // const person = {
 //   name: 'Max',
@@ -14,6 +14,8 @@ function startGame() {
 // person.greet()
 // 객체에 대한 함수를 메서드라고 부른다
 // 함수는 객체이다
-console.dir(startGame)
+// console.dir(startGame)
 
-startGameBtn.addEventListener('click', startGame)
+startGameBtn.addEventListener('click', function startGame(){
+  console.log('Game is Striting...', age)
+})
