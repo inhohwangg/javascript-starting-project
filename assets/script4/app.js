7305d291d850bcf496b7a437b554b05e463191a9
inhohwangg <sk1440sk@naver.com>
2022-09-15 23:44:56 +0900

    add

--- assets/script4/app.js
+++ assets/script4/app.js
@@ -113,8 +113,8 @@ const sortedPrices = prices.sort((a, b) => {
 // console.log(sortedPrices.reverse());
 console.log(sortedPrices);
 
-const filteredArray = prices.filter((price, idx, prices)=> {
-    return price > 6
+const filteredArray = prices.filter((price, idx, prices) => {
+  return price > 6;
 });
 
-console.log(filteredArray)
+console.log(filteredArray);
