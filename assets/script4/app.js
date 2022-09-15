ae2c4e3cb1efa30578028d25d5651847a5509026
inhohwangg <sk1440sk@naver.com>
2022-09-15 21:20:44 +0900

    add

--- assets/script4/app.js
+++ assets/script4/app.js
@@ -29,20 +29,28 @@
 
 // console.log(personalData[1])
 
-const hobbies = ["Sports", "Cooking"];
-hobbies.push("Reading");
-console.log(hobbies);
-hobbies.unshift("Coding");
-const poppedValue = hobbies.pop();
-hobbies.shift();
-console.log(hobbies);
+// const hobbies = ["Sports", "Cooking"];
+// hobbies.push("Reading");
+// console.log(hobbies);
+// hobbies.unshift("Coding");
+// const poppedValue = hobbies.pop();
+// hobbies.shift();
+// console.log(hobbies);
 
-hobbies[1] = "Coding";
-// hobbies[5] = "Reading";
-console.log(hobbies, hobbies[4]);
+// hobbies[1] = "Coding";
+// // hobbies[5] = "Reading";
+// console.log(hobbies, hobbies[4]);
 
-hobbies.splice(1, 0, 'Good Food')
-console.log(hobbies)
+// hobbies.splice(1, 0, 'Good Food')
+// console.log(hobbies)
 
-const removedElements =  hobbies.splice(-2, 1)
-console.log(hobbies)
+// const removedElements =  hobbies.splice(-2, 1)
+// console.log(hobbies)
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+// const storeResult = testResults.slice(2);
+const storeResult = testResults.concat([3.99, 2]);
+
+testResults.push(5.91);
+
+console.log(storeResult, testResults);
