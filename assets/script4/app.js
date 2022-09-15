80c7bd29e779001b1984c0d3c8eaccf2f10e07ad
inhohwangg <sk1440sk@naver.com>
2022-09-15 22:18:01 +0900

    add

--- assets/script4/app.js
+++ assets/script4/app.js
@@ -93,13 +93,9 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-// for (const price of prices) {
-//   taxAdjustedPrices.push(price * (1 + tax));
-// }
-
-prices.forEach((price, idx, prices) => {
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
-  taxAdjustedPrices.push(priceObj);
+  return priceObj;
 });
 
-console.log(taxAdjustedPrices);
+console.log(prices, taxAdjustedPrices);
