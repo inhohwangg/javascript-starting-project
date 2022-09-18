0865889a8a733b199cf1c5608bd5e5c20e99e9d3
inhohwangg <sk1440sk@naver.com>
2022-09-18 11:25:56 +0900

    code add

--- assets/script5/object.js
+++ assets/script5/object.js
@@ -1,10 +1,21 @@
-const person = {
-    name: 'Max',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    greet: function() {
-        alert('Hi there!')
-    }
-}
+const movieList = document.getElementById("movie-list");
 
-person.greet()
+movieList.style['backgroundColor'] = "red";
+movieList.style.display = "block";
+
+let person = {
+  "first name": "Max",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  greet: function () {
+    alert("Hi there!");
+  },
+};
+
+delete person.age;
+// person.age = undefined;
+// person.age = null
+
+person.isAdmin = true;
+
+console.log(person["first name"]);
