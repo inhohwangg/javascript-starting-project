16656d5b0f26a0cad76e8a04f0d0ea49c5b98693
inhohwangg <sk1440sk@naver.com>
2022-09-22 23:57:20 +0900

    class code add

--- assets/script6/app.js
+++ assets/script6/app.js
@@ -17,7 +17,7 @@ class ShoppingCart {
 
   addProduct(product) {
     this.item.push(product);
-    this.totalOutput = `<h2>Total: \$${1}</h2>`;
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
   }
 
   render() {
@@ -38,8 +38,7 @@ class ProductItem {
   }
 
   addToCart() {
-    console.log("Adding product to cart...");
-    console.log(this.product);
+    App.addProductToCart(this.product)
   }
 
   render() {
@@ -96,8 +95,8 @@ class Shop {
   render() {
     const renderHook = document.getElementById("app");
 
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
 
@@ -110,6 +109,10 @@ class App {
   static init() {
     const shop = new Shop();
     shop.render();
+    this.cart = shop.cart
+  }
+  static addProductToCart(product) {
+    this.cart.addProduct(product)
   }
 }
 
